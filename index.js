82571153e51382a4b60b6062245fcece35df234a
Suhit Kumar <suhit.kmr1@gmail.com>
2018-07-04 05:00:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function multiply(a,b) {
 function divide(a,b) {
   return (a/b)
 }
+function inc(n) {
+  n++;
+  return (n)
+}
+function dec(n) {
+  n--;
+  return (n)
+}
+function makeInt(n) {
+  
+}
